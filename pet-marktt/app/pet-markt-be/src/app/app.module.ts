2e26bfdc202b36e8661be37780ad42c808644c3d
Jew Larbi Danquah <100996700+mrrjew@users.noreply.github.com>
2025-06-19 18:59:27 +0000

    localtunnel

---
+++ pet-marktt/app/pet-markt-be/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from "../products/products.module"
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule} from '@nestjs/graphql';
+
+@Module({
+  imports: [
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: true,
+    playground: true,
+    introspection: true,
+}),
+ProductsModule
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ pet-marktt/app/pet-markt-be/src/products/products.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductInput } from './dto/create-product.input';
+import { UpdateProductInput } from './dto/update-product.input';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+  create(createProductInput: CreateProductInput) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async searchProducts(term: string): Promise<any[]> {
+    const lowercaseTerm = term.toLowerCase();
+    return this.prisma.product.findMany({
+      where: {
+        OR: [
+          { name: { contains: lowercaseTerm, mode: 'insensitive' } },
+          { description: { contains: lowercaseTerm, mode: 'insensitive' } },
+        ],
+      },
+    });
+  }
+
+  update(id: number, updateProductInput: UpdateProductInput) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
